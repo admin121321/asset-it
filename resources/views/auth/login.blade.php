3ddf588558fa3214543d214c359a85c1726b25e6
sudimanw <dodosudi@yahoo.co.id>
2023-05-03 17:54:36 +0700

    first commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+
+@section('content')
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                  <img src="assets/images/logo-ati.png" style="width:100px;height:100px;" class="logo d-flex align-items-center w-auto">
+              </div><!-- End Logo -->
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+                @guest
+                 @if (Route::has('login'))
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                    <p class="text-center small">Enter your username & password to login</p>
+                  </div>
+
+                  <form class="row g-3 needs-validation" method="POST" action="{{ route('login') }}"  novalidate>
+                     @csrf
+                        <div class="col-12">
+                            <label for="yourUsername" class="form-label">Email</label>
+                            <div class="input-group has-validation">
+                                <span class="input-group-text" id="inputGroupPrepend"></span>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <label for="yourPassword" class="form-label">Password</label>
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+
+                        <div class="col-12">
+                            <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="form-check-label" for="rememberMe">Remember me</label>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                            <button class="btn btn-primary w-100" type="submit">Login</button>
+                        </div>
+                        <div class="col-12">
+                            <p class="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
+                        </div>
+                  </form>
+                  @endif
+                  @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Anda Sudah Login</h5>
+                    <p class="text-center small">Silahkan Ke Dashboard</p>
+                </div>
+                    <a class="btn btn-primary text-center d-flex align-items-center w-auto" href="{{ url('/home') }}">
+                        <b>------Dashboard------</b>
+                    </a>
+                        @endguest
+                </div>
+              </div>
+
+              <div class="credits">
+                Designed by <a href="#">ATI</a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+<!-- End #main -->
+@endsection
---
+++ resources/views/layouts/app-backend.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @extends('layouts.css')
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="loader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+            </svg>
+        </div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+    
+    @include('tampilan.navigation')
+    @include('tampilan.sidebar')
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    @include('tampilan.footer')
+    </div>
+    @extends('layouts.js')
+</body>
+</html>
+
