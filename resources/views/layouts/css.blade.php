d51550b1aff6fe363e47855d9e828d086fa5db1f
unknown <sudimanw@gmail.com>
2023-05-04 17:25:30 +0700

    Create Menu Navigation

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,9 +1,10 @@
    <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}">
     <!-- Pignose Calender -->
-    <link href="{{ asset('assets/plugins/pg-calendar/css/pignose.calendar.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/pg-calendar/css/pignose.calendar.min.css') }}" >
     <!-- Chartist -->
     <link rel="stylesheet" href="{{ asset('assets/plugins/chartist/css/chartist.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css') }}">
     <!-- Custom Stylesheet -->
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" >
+    <!-- <link rel="stylesheet" href="{{ asset('assets/font-awesome/css/font-awesome.min.css') }}"> -->
