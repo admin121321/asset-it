29608950837250d8627a8568f313e016e7e7914f
sudimanw <dodosudi@yahoo.co.id>
2023-05-14 09:06:00 +0700

    CRUD Aksesoris Pengguna

--- app/Http/Controllers/AksesorisPenggunaController.php
+++ app/Http/Controllers/AksesorisPenggunaController.php
@@ -16,8 +16,118 @@ use App\Models\AksesorisPengguna;
 class AksesorisPenggunaController extends Controller
 {
     //
-    public function index()
-    {
+    public function index(Request $request)
+     {
+        if ($request->ajax()) {
+            $data = AksesorisPengguna::join('desktop_device', 'desktop_device.id', '=', 'aksesoris_pengguna.desktop_id')
+                                    ->join('aksesoris_device', 'aksesoris_device.id', '=', 'aksesoris_pengguna.aksesoris_id')
+                                    ->select('aksesoris_pengguna.*', 'aksesoris_device.brand_aksesoris', 'aksesoris_device.model_aksesoris', 'aksesoris_device.type_aksesoris','desktop_device.brand_desktop') 
+                                    ->get();
+            return Datatables::of($data)->addIndexColumn()
+                ->addColumn('desktop_id', function($data){
+                    return $data->brand_desktop;
+                })
+                ->addColumn('aksesoris_id', function($data){
+                    return $data->model_aksesoris;
+                })
+                ->addColumn('aksesoris_id', function($data){
+                    return $data->type_aksesoris;
+                })
+                ->addColumn('action', function($data){
+                    $button = '<button type="button" name="edit" id="'.$data->id.'" class="edit btn btn-primary btn-sm"> <i class="bi bi-pencil-square"></i>Ubah</button>';
+                    $button .= '<button type="button" name="edit" id="'.$data->id.'" class="delete btn btn-danger btn-sm"> <i class="bi bi-backspace-reverse-fill"></i> Hapus</button>';
+                    $button .= '<button type="button" name="edit" id="'.$data->id.'" class="detailButton btn btn-success btn-sm"> <i class="bi bi-pencil-square"></i>Detail</button>';
+                    return $button;
+                })
+                ->make(true);
+        }
+        // $data = AksesorisPengguna::get();
+        // dd ($data);
         return view('aksesoris-pengguna.index');
+     }
+    
+     public function store(Request $request)
+    {
+        $rules = array(
+            // 'id'            =>  'required',
+            'desktop_id'   =>  'required',
+            'aksesoris_id'=>  'required',
+            'qty'       =>  'required',
+        );
+ 
+        $error = Validator::make($request->all(), $rules);
+ 
+        if($error->fails())
+        {
+            return response()->json(['errors' => $error->errors()->all()]);
+        }else{
+
+            AksesorisPengguna::create($request->all());
+            $form_data = AksesorisDevice::findOrFail($request->aksesoris_id);
+            $form_data->stok -= $request->qty;
+            $form_data->save();
+           
+
+            return response()->json(['success' => 'Data Added successfully.']);
+        }
+    }
+    
+    public function edit($id)
+    {
+        if(request()->ajax())
+        {
+            $data = AksesorisPengguna::findOrFail($id);
+            return response()->json(['result' => $data]);
+        }
+    }
+    
+    public function update(Request $request, AksesorisPengguna $aksesoris_pengguna)
+    {
+        $rules = array(
+            'desktop_id'   =>  'required',
+            // 'lisensi_id'=>  'required',
+            // 'qty'       =>  'required',
+        );
+ 
+        $error = Validator::make($request->all(), $rules);
+ 
+        if($error->fails())
+        {
+            return response()->json(['errors' => $error->errors()->all()]);
+        }
+        $aksesoris_pengguna = AksesorisPengguna::find($request->hidden_id);
+            if($aksesoris_pengguna->qty='1'){
+                $aksesoris_pengguna->update([
+                    'desktop_id'   =>  $request->desktop_id,
+                    // 'printer_id'=>  $request->printer_id,
+                ]);
+            }
+            else{    
+                $aksesoris_pengguna->update($request->all());
+                $form_data = AksesorisDevice::findOrFail($request->aksesoris_id);
+                $form_data->stok -= $request->qty;
+            }
+ 
+        return response()->json([
+            'success' => 'Data Berhasil di Update',
+            
+        ]);
+    }
+
+    public function detail($id)
+    {
+        if(request()->ajax())
+        {
+            $data = AksesorisPengguna::findOrFail($id);
+            return response()->json($data);
+        }
+    }
+
+    public function destroy($id)
+    {
+        // hapus file
+        AksesorisPengguna::where('id',$id)->delete();
+		return redirect()->back();
+        
     }
 }
--- resources/views/aksesoris-pengguna/index.blade.php
+++ resources/views/aksesoris-pengguna/index.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app-backend')
-
 @section('content')
- <!--**********************************
+<!--**********************************
             Content body start
         ***********************************-->
         <div class="content-body">
-
             <div class="row page-titles mx-0">
                 <div class="col p-md-0">
                     <ol class="breadcrumb">
@@ -15,13 +13,106 @@
                 </div>
             </div>
             <!-- row -->
-
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body">
                                 <p>Aksesoris Pengguna</p>
+                                <!-- DataTables -->
+                                <div align="right">
+                                    <button type="button" name="create_record" id="create_record" class="btn btn-success"> <i class="bi bi-plus-square"></i> Add</button>
+                                </div>
+                                <div class="section-body">
+                                    <!-- card-body -->
+                                    <div class="row">
+                                        <div class="col-12 table-responsive">
+                                        <br />
+                                            <table class="table table-striped table-bordered zero-configuration printerpengguna_datatable"> 
+                                                <thead>
+                                                    <tr>
+                                                        <th>Brand Device</th>
+                                                        <th>Type Aksesoris</th>
+                                                        <th>Model Aksesoris</th>
+                                                        <th>Qty</th>
+                                                        <th width="180px">Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody></tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <!-- card-body -->
+                                    <!-- Modal -->
+                                    <!-- input and Update -->
+                                    <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                <form method="post" id="sample_form" enctype="multipart/form-data" class="form-horizontal">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="ModalLabel">Tambah Printer</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <span id="form_result"></span>
+                                                        <div class="form-group">
+                                                            <label>ID Pengguna: </label>
+                                                            <select class="form-control" id="desktop_id" name="desktop_id" aria-label="Floating label select example">
+                                                                <option>--Pilih Pengguna--</option>
+                                                                @foreach(App\Models\DesktopDevice::all() as $desktop)
+                                                                <option value="{{ $desktop->id}}" id="desktop_id">{{ $desktop->sn_desktop }} - {{ $desktop->model_desktop}}</option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label>Printer: </label>
+                                                            <select class="form-control" id="aksesoris_id" name="aksesoris_id" aria-label="Floating label select example">
+                                                                <option>--Pilih Aksesoris--</option>
+                                                                @foreach(App\Models\AksesorisDevice::all() as $aksesoris)
+                                                                    @if ($aksesoris->stok=='0')
+                                                                    @else
+                                                                    <option value="{{ $aksesoris->id}}" id="aksesoris_id">{{ $aksesoris->model_aksesoris }} - {{ $aksesoris->sn_aksesoris }}</option>
+                                                                    @endif
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                        <div class="form-group" hidden>
+                                                            <label>Qty: </label>
+                                                            <input type="text" name="qty" id="qty" value="1" class="form-control" />
+                                                        </div>
+                                                        <input type="hidden" name="action" id="action" value="Add" />
+                                                        <input type="hidden" name="hidden_id" id="hidden_id" />
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        <input type="submit" name="action_button" id="action_button" value="Add" class="btn btn-info" />
+                                                    </div>
+                                                </form>  
+                                                </div>
+                                            </div>
+                                        </div>
+                        
+                                        <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <form method="post" id="sample_form" class="form-horizontal">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="ModalLabel">Confirmation</h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">X</button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <h4 align="center" style="margin:0;">Are you sure you want to remove this data?</h4>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                            <button type="button" name="ok_button" id="ok_button" class="btn btn-danger">OK</button>
+                                                        </div>
+                                                    </form>  
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div> 
+                                <!-- End DataTabels -->
                             </div>
                         </div>
                     </div>
@@ -32,4 +123,203 @@
         <!--**********************************
             Content body end
         ***********************************-->
+         <!-- detail -->
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="fModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <span id="detail_result"></span>
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Detail Lisensi Pengguna</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p><strong>ID Desktop:</strong><span id="desktop-id"></span></p>
+                <p><strong>Lisensi ID:</strong> <span id="aksesoris-id"></span></p>
+                <p><strong>Qty:</strong> <span id="qtY"></span></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+
+<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    var table = $('.printerpengguna_datatable').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: "{{ route('aksesoris-pengguna.index') }}",
+        columns: [
+            {data: 'brand_desktop', name: 'brand_dektop'},
+            {data: 'type_aksesoris', name: 'type_aksesoris'},
+            {data: 'model_aksesoris', name: 'model_aksesoris'},
+            {data: 'qty', name: 'qty'},
+            {data: 'action', name: 'action', orderable: false, searchable: false},
+        ]
+    });
+ 
+    $('#create_record').click(function(){
+        $('.modal-title').text('Add New Record');
+        $('#action_button').val('Add');
+        $('#action').val('Add');
+        $('#form_result').html('');
+        $('#formModal').modal('show');
+    });
+ 
+    $('#sample_form').on('submit', function(event){
+        event.preventDefault();
+        var formData = new FormData($(this)[0]); 
+        var action_url = '';
+ 
+        if($('#action').val() == 'Add')
+        {
+            action_url = "{{ route('aksesoris-pengguna.store') }}";
+        }
+ 
+        if($('#action').val() == 'Edit')
+        {
+            action_url = "{{ route('aksesoris-pengguna.update') }}";
+        }
+ 
+        $.ajax({
+            type: 'post',
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            url: action_url,
+            data:$(this).serialize(),
+            dataType: 'json',
+            processData: false,  // Important!
+            contentType: false,
+            cache: false,
+            data: formData,
+            success: function(data) {
+                console.log('success: '+data);
+                var html = '';
+                if(data.errors)
+                {
+                    html = '<div class="alert alert-danger">';
+                    for(var count = 0; count < data.errors.length; count++)
+                    {
+                        html += '<p>' + data.errors[count] + '</p>';
+                    }
+                    html += '</div>';
+                }
+                if(data.success)
+                {
+                    html = '<div class="alert alert-success">' + data.success + '</div>';
+                    $('#sample_form')[0].reset();
+                    $('#printerpengguna_datatable').DataTable().ajax.reload();
+                    window.location.reload();
+                }
+                $('#form_result').html(html);
+            },
+            error: function(data) {
+                var errors = data.responseJSON;
+                console.log(errors);
+            }
+        });
+    });
+
+    $(document).on('click', '.edit', function(event){
+        event.preventDefault(); 
+        var id = $(this).attr('id'); alert(id);
+        $('#form_result').html('');
+
+        $.ajax({
+            url :"/aksesoris-pengguna/edit/"+id+"/",
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            dataType:"json",
+            processData: false,  
+            contentType: false,
+            cache: false,
+            success:function(data)
+            {
+                console.log('success: '+data);
+                // tinyMCE.activeEditor.setContent(data.result.deskripsi);
+                $('#desktop_id').val(data.result.desktop_id);
+                $('#aksesoris_id').val(data.result.aksesoris_id);
+                $('#qty').val(data.result.qty);
+                $('#hidden_id').val(id);
+                $('.modal-title').text('Edit Record');
+                $('#action_button').val('Update');
+                $('#action').val('Edit'); 
+                $('.editpass').hide(); 
+                $('#formModal').modal('show');
+            },
+            error: function(data) {
+                var errors = data.responseJSON;
+                console.log(errors);
+                setTimeout(function(){
+                $('#confirmModal').modal('hide');
+                alert('Data Tidak Berhasil Diupdate');
+                }, 2000);
+                window.location.reload();
+            }
+        })
+    });
+
+    var id;
+ 
+    $(document).on('click', '.delete', function(){
+        id = $(this).attr('id');
+        $('#confirmModal').modal('show');
+    });
+ 
+    $('#ok_button').click(function(){
+        $.ajax({
+            url:"aksesoris-pengguna/destroy/"+id,
+            beforeSend:function(){
+                $('#ok_button').text('Deleting...');
+            },
+            success:function(data)
+            {
+                setTimeout(function(){
+                $('#confirmModal').modal('hide');
+                $('#printerpengguna_datatable').DataTable().ajax.reload();
+                alert('Data Deleted');
+                }, 2000);
+                window.location.reload();
+            }
+        })
+    });
+    
+    // detail
+    $(document).on('click', '.detailButton', function(event){
+        event.preventDefault(); 
+        var id = $(this).attr('id'); alert(id);
+        $('#detail_result').html('');
+
+        $.ajax({
+            url :"/aksesoris-pengguna/detail/"+id+"/",
+            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+            dataType:"json",
+            processData: false,  
+            contentType: false,
+            cache: false,
+            success:function(data)
+            {
+                
+                $('#desktop-id').text(data.desktop_id);
+                $('#aksesoris-id').text(data.aksesoris_id);
+                $('#qtY').text(data.qty);
+                $('.modal-title').text('Detail');
+                $('#fModal').modal('show');
+
+                // console.log(
+                //     'desktop-id: '+data.desktop_id,
+                //     'lisensi-id:'+ data.lisensi_id,
+                //     'qtY:'+ data.qty,
+                //      $('#user-id')
+                //     );
+                
+            },
+        })
+    });
+
+});
+</script>
 @endsection
